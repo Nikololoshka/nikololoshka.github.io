63f85893ea9028dde2e750d66e77d43a8c0dc1ec
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2022-02-06 22:33:43 +0300

    added a little description

--- src/components/stankin-schedule-editor/StankinScheduleEditor.tsx
+++ src/components/stankin-schedule-editor/StankinScheduleEditor.tsx
@@ -10,7 +10,19 @@ const StankinScheduleEditor = () => {
             <Typography variant='h4' gutterBottom>
                 Stankin Schedule Editor
             </Typography>
-            
+
+            <Typography paragraph>
+                <b>StankinScheduleEditor2</b> - не официальный редактор и генератор
+                еженедельного расписания для МГТУ "СТАНКИН".
+            </Typography>
+
+            <Typography paragraph>
+                Цель создания проекта - это возможность перевода расписаний из PDF в JSON с
+                последующей генерации альтернативного (еженедельного, с дополнениями и т.п.)
+                расписания или использование получаемых JSON в других
+                программах (например, при создании расписания для Google Calendar).
+            </Typography>
+
         </ContentBox>
     )
 }
